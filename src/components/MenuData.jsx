23dc28acbae4af4a7fee35ab10bda440d6cd2d72
mlaane <goobric@gmail.com>
2024-06-04 14:47:09 +0100

    all files for navbar

---
+++ src/components/MenuData.jsx
@@ -0,0 +1,38 @@
+export const MenuData = [
+  {
+    title: "Home",
+    url: "#",
+    cName: "nav-links",
+    icon: "fa-solid fa-house-user",
+  },
+  {
+    title: "How It Works",
+    url: "#",
+    cName: "nav-links",
+    icon: "fa-solid fa-person-chalkboard",
+  },
+  {
+    title: "About Us",
+    url: "#",
+    cName: "nav-links",
+    icon: "fa-solid fa-circle-info",
+  },
+  {
+    title: "Our Specialists",
+    url: "#",
+    cName: "nav-links",
+    icon: "fa-solid fa-briefcase",
+  },
+  {
+    title: "Need Help?",
+    url: "#",
+    cName: "nav-links",
+    icon: "fa-solid fa-handshake-angle",
+  },
+  {
+    title: "Login",
+    url: "#",
+    cName: "nav-links-login",
+    icon: "fa-solid fa-arrow-right-to-bracket",
+  },
+];
